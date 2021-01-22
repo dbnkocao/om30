7a65c33be958ae64d0c6d3560a692e4a53722df1
Daniel Nascimento <dbnkocao@gmail.com>
2021-01-22 12:32:44 -0300

    adding municipe model and migration

---
+++ db/migrate/20210122150925_create_municipes.rb
@@ -0,0 +1,13 @@
+class CreateMunicipes < ActiveRecord::Migration[6.1]
+  def change
+    create_table :municipes do |t|
+      t.string :cpf
+      t.string :email
+      t.string :dt_nasc
+      t.string :telefone
+      t.integer :status
+
+      t.timestamps
+    end
+  end
+end
