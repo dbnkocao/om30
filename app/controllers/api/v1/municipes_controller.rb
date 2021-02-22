5226b3b641f349dd3f39e0683edd432dc70ab8a6
Daniel Nascimento <dbnkocao@gmail.com>
2021-02-22 17:30:49 -0300

    feat(municipe update): adding route and action to update municipe

--- app/controllers/api/v1/municipes_controller.rb
+++ app/controllers/api/v1/municipes_controller.rb
@@ -2,6 +2,7 @@ module Api
   module V1
     class MunicipesController < ApplicationController
       skip_before_action :verify_authenticity_token
+      before_action :set_municipe, only: [:show, :update]
 
       def index
         render json: Municipe.all
@@ -20,7 +21,19 @@ module Api
         end
       end
 
+      def update
+        if @municipe.update(municipe_params)
+          render json: { message: "Munícipe atualizado com sucesso!" }, status: :created
+        else
+          render json: { errors: @municipe.errors.full_messages }, status: :unprocessable_entity
+        end
+      end
+
       private
+
+      def set_municipe
+        @municipe = Municipe.find(params[:id])
+      end
 
       def municipe_params
         params.require(:municipe).permit(:cpf, :email, :dt_nasc, :telefone, :nome, :foto,
